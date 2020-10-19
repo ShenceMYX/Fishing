6be531694952d542c93d4955f38d544d36080c5c
xdd44-utopia <xdd44.utopia@gmail.com>
2020-10-19 10:15:24 +0800

    realize 'hand grab and release oar'

---
+++ Fishing 1.8/Assets/Script/Self/Hand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+	
+	private float speed = 1f;
+	void Start()
+	{
+				
+	}
+
+	void Update()
+	{
+		
+		float transV = Input.GetAxis("Vertical") * speed;
+		float transH = Input.GetAxis("Horizontal") * speed;
+		float transJ = Input.GetAxis("Jump") * speed;
+
+		transV *= Time.deltaTime;
+		transH *= Time.deltaTime;
+		transJ *= Time.deltaTime;
+
+		transform.Translate(transH, transJ, transV);
+
+	}
+}
---
+++ Fishing 1.8/Assets/Script/Self/Oar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oar : MonoBehaviour
+{
+	public GameObject rightHand;
+	private bool isHandEntered = false;
+	private GameObject entered;
+	private bool isGrabbed = false;
+	void Start()
+	{
+				
+	}
+
+	void Update()
+	{
+		
+		if (Input.GetMouseButtonDown(0)) {
+			if (!isGrabbed && isHandEntered && entered == rightHand) {
+				this.transform.SetParent(entered.transform);
+				isGrabbed = true;
+				Debug.Log("grabbed");
+			}
+			else if (isGrabbed) {
+				this.transform.parent = null;
+				isGrabbed = false;
+				isHandEntered = false;
+			}
+		}
+
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		isHandEntered = true;
+		entered = other.gameObject;
+		Debug.Log("Entering");
+	}
+
+	private void OnTriggerExit (Collider other) {
+		isHandEntered = false;
+		Debug.Log("Exiting");
+	}
+
+}
